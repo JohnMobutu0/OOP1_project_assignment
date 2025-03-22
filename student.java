bc737aea9db651c4aee406d51ce88b6ebef94f71
Mobutu <mulumeoderhwa.cirhuza@strathmore.edu>
2025-03-22 13:00:02 +0300

    number two

---
+++ student.java
@@ -0,0 +1,19 @@
+public class student {
+    // The class definition
+        String name;
+        int id;
+
+        // Constructor to initialize student data
+        public student(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        // Method to display student info
+        public void displayInfo() {
+            System.out.println("Name: " + name);
+            System.out.println("ID: " + id);
+        }
+    }
+
+
